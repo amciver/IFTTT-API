8b2f3b5d5a87be2aa85b2dfdc01155f444eecb63
Aaron McIver <aaron.mciver@gmail.com>
2016-08-16 21:17:17 -0700

    Added azure storage configuration; validating insertion/connection

--- routes/runscope.js
+++ routes/runscope.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//set up azure storage
+var azure = require('azure-storage');
+var tableSvc = azure.createTableService();
+var entityGenerator = azure.TableUtilities.entityGenerator;
+
 /* GET runscope definition. */
 router.get('/', function(req, res) {
+  
+var task = {
+              PartitionKey: entityGenerator.String('status'),
+              RowKey: entityGenerator.String('1'),
+              data: entityGenerator.String('available'),
+             //dueDate: entityGenerator.DateTime(new Date(Date.UTC(2015, 6, 20))),
+            };
+tableSvc.insertEntity(,task, function (error, result, response) {
+  if(!error){
+    // Entity inserted
+  }
+});
   res.send('This is the runscope API');
 });
 
