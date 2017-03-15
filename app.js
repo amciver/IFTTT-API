7c42f6bc8023bb999468959971b681d6e54bc0c8
Aaron McIver <aaron.mciver@gmail.com>
2017-03-15 09:51:15 -0700

    Removed reference to route no longer in use

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/ifttt', routesIFTTT);
-app.use('/messaging/subscription', routesMessaging);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
