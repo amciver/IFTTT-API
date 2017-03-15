9fc38d438b42696b28db14aaf4e575645f6e3aa3
Aaron McIver <aaron.mciver@gmail.com>
2017-03-15 07:01:18 -0700

    Refactor of manual fetch of a single message

--- routes/messaging/subscription.js
+++ routes/messaging/subscription.js
@@ -19,76 +19,23 @@ var subscription = "ifttt_messages_subscription";
 
 var serviceBusService = azure.createServiceBusService(connectionString);
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'BMW IFTTT SUBSCRIPTION' });
-// });
-
-// router.get('/v1/messages/purge', function(req, res, next) {
-//   res.sendStatus(200);
-// });
-
 /* GET messages. */
-router.get('/v1/messages/retrieve', function(req, res, next) {
-    checkMessages(res);
-//getMessages(res);
-//res.sendStatus(200);
-});
+router.get('/v1/messages/retrieve', function (req, res, next) {
+    serviceBusService.receiveSubscriptionMessage(topic, subscription, function (error, receivedMessage) {
+        if (!error) {
+            var messageId = receivedMessage.brokerProperties.MessageId
+            console.log("message [" + messageId + "] being returned [" + JSON.stringify(receivedMessage) + "]")
 
-var checkMessages = function (res) {
-    //serviceBusService.receiveSubscriptionMessage(topic, subscription)
-        serviceBusService.receiveSubscriptionMessage(topic, subscription, function (error, receivedMessage) {
-            if (!error) {
-                var messageId = receivedMessage.brokerProperties.MessageId
-
-        console.log("message [" + messageId + "] received [" + JSON.stringify(receivedMessage) + "]")
-        
-        var message = JSON.parse(receivedMessage.body)
-        
-        console.log("parsed message body ["+JSON.stringify(message)+"]")
-
-        var speed = message.speed
-        var triggeredTime = message.triggeredTime
-
-        console.log("data extracted speed:[" + speed + "], triggeredTime:[" + triggeredTime + "]")
-
-        var task = {
-            PartitionKey: entityGenerator.String(storagePartition),
-            RowKey: entityGenerator.String(uuid.v4()),
-            MessageId: entityGenerator.String(messageId),
-            Data: entityGenerator.String(speed),
-            Triggered: entityGenerator.String(triggeredTime)
+            res.status(200).send(receivedMessage);
         }
-        console.log("task created for table insertion [" + JSON.stringify(task) + "]")
-
-        tableSvc.insertEntity(storageTable, task, function (error) {
-            if (!error) {
-                console.log("message [" + task.MessageId.MessageId._ + "] successfully processed + inserted")
-                res.status(200).send("{\"messageId\":\"" + task.MessageId._ + "\"}")
-            }
-            else {
-                console.log("message [" + task.MessageId._ + "] processed; insertion failed")
-                console.error(error)
-                res.sendStatus(500)
-            }
-        })
-    }
-    else {
-        console.error(error)
-    }
+        else {
+            console.error(error)
+            res.status(500).send(error)
         }
-        )};
 
+        //res.status(200).send("{\"messageId\":\"" + task.MessageId._ + "\"}")
+        //var message = JSON.parse(receivedMessage.body)
 
-var processError = function (reason) {
-    console.log("Error:");
-    console.log(reason);
-}
-
-var setNextCheck = function () {
-    setTimeout(checkMessages, 1000);
-}
-
-//checkMessages();
+    })});
 
 module.exports = router;
