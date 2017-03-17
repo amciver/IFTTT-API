fedbc774744295f13c4aa0eb01ceaea127b49460
Aaron McIver <aaron.mciver@gmail.com>
2017-03-17 09:41:30 -0700

    Added additional logging

--- routes/api/ifttt.js
+++ routes/api/ifttt.js
@@ -13,71 +13,72 @@ var subscription = "ifttt_messages_subscription";
 //make sure that the topic exists and was not removed; creation taking place within portal
 var serviceBusService = azure_sb.createServiceBusService(connectionStringManage);
 serviceBusService.createTopicIfNotExists(topic, function (error) {
-  if (!error) {
-    console.log('topic [' + topic + '] successfully created and/or already exists');
-  }
-  else {
-    console.error('topic [' + topic + '] creation has failed; [' + error + ']');
-  }
+    if (!error) {
+        console.log('topic [' + topic + '] successfully created and/or already exists');
+    }
+    else {
+        console.error('topic [' + topic + '] creation has failed; [' + error + ']');
+    }
 })
 
 /* ROUTES DEFS  --------------------------------------------------------------------------*/
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'IFTTT' });
+    console.log('entered route /');
+    res.render('index', { title: 'IFTTT' });
 });
 
 /* GET IFTTT definition */
 router.get('/v1', function (req, res) {
-  res.send('You are using v1 of the IFTTT API');
+    res.send('You are using v1 of the IFTTT API');
 });
 
 /* GET IFTTT API status */
 router.get('/v1/status', function (req, res) {
-  res.json({ "status": "available" });
+    res.json({ "status": "available" });
 });
 
 /* GET message previously posted to IFTTT API */
 router.get('/v1/message', function (req, res, next) {
-  serviceBusService.receiveSubscriptionMessage(topic, subscription,
-    function (error, receivedMessage) {
-      if (!error) {
-        var messageId = receivedMessage.brokerProperties.MessageId
-        console.log("message [" + messageId + "] being returned [" + JSON.stringify(receivedMessage) + "]")
-        res.status(200).send(receivedMessage);
-      }
-      else {
-        console.error(error)
-        res.status(500).send(error)
-      }
-    })
+    serviceBusService.receiveSubscriptionMessage(topic, subscription,
+        function (error, receivedMessage) {
+            if (!error) {
+                var messageId = receivedMessage.brokerProperties.MessageId
+                console.log("message [" + messageId + "] being returned [" + JSON.stringify(receivedMessage) + "]")
+                res.status(200).send(receivedMessage);
+            }
+            else {
+                console.error(error)
+                res.status(500).send(error)
+            }
+        })
 });
 
 /* POST message to IFTTT API */
 router.post('/v1/speed', function (req, res) {
-  var speed = req.body.speed;
-  var triggeredTime = req.body.triggered;
-  var message = {
-    body: "{\"speed\":\"" + speed + "\", \"triggeredTime\":\"" + triggeredTime + "\"}",
-    brokerProperties: {
-      MessageId: uuid.v4()
+    var speed = req.body.speed;
+    var triggeredTime = req.body.triggered;
+    var message = {
+        body: "{\"speed\":\"" + speed + "\", \"triggeredTime\":\"" + triggeredTime + "\"}",
+        brokerProperties: {
+            MessageId: uuid.v4()
+        }
     }
-  }
 
-  console.log("posting message [" + message.brokerProperties.MessageId + "] with body [" + message.body + "]")
+    console.log("posting message [" + message.brokerProperties.MessageId + "] with body [" + message.body + "]")
 
-  //place a message on service bus to get consumed
-  serviceBusService.sendTopicMessage(topic, message, function (error) {
-    if (error) {
-      console.log("error sending message [" + message.brokerProperties.MessageId + "]");
-      console.error(error);
-    }
-    else {
-      console.log("message [" + message.brokerProperties.MessageId + "] successfully posted");
-      res.sendStatus(200);
-    }
-  });
+    //place a message on service bus to get consumed
+    serviceBusService.sendTopicMessage(topic, message, function (error) {
+        if (error) {
+            console.log("error sending message [" + message.brokerProperties.MessageId + "]");
+            console.error(error);
+        }
+        else {
+            console.log("message [" + message.brokerProperties.MessageId + "] successfully posted");
+            res.sendStatus(200);
+        }
+    });
 });
 
 module.exports = router;
